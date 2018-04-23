3d894cdbf3f10b06321be0f8a3011169afd6462f
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-04-23 13:43:36 -0500

    update

--- RandomNumbersWith5TimesReplay/RandomNumbersWith5TimesReplay/Source.cpp
+++ RandomNumbersWith5TimesReplay/RandomNumbersWith5TimesReplay/Source.cpp
@@ -1,3 +1,39 @@
-//
-// 
-// Author:
+//Program: Random Number Game With Levels
+//Date: 4-10-2018
+//Author: Daniel McGlasson
+
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int numberChoice;
+	int secret;
+	int count = 0;
+
+	srand(time(NULL));
+
+	secret = rand() % 10 + 1;
+	do{
+		std::cout << "Please pick a number between 1 - 100" << std::endl;
+		std::cin >> numberChoice;
+		if (numberChoice > secret)
+		{
+			std::cout << "The number you chose is to high!, guess agian loser" << std::endl;
+			count = count + 1;
+			//count++//
+		}
+		else if (numberChoice < secret)
+		{
+			std::cout << "The number you have chose is to low!, guess again loser" << std::endl;
+			count = count + 1;
+		}
+	} while (numberChoice != secret);
+	std::cout << "Congrats! " << std::endl;
+	std::cout << "It took you " << count << " tries to get it correct." << std::endl;
+
+	system("pause");
+	return 0;
+}
