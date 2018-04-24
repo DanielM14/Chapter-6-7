caeaa0ee728ef9420f7d589e4e32f7ce1b29e583
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-04-24 13:31:56 -0500

    u

--- Computer Guesses Number/Computer Guesses Number/Source.cpp
+++ Computer Guesses Number/Computer Guesses Number/Source.cpp
@@ -1,11 +1,87 @@
-// Computer Guessing a Number
-// 4-24-18
-// Daniel McGlasson
+//
+//
+// Daniel McGlasson 
 
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
+	srand(static_cast<unsigned int>(time(0)));
+	int userTries = 0;
+	int userNumber;
+	bool error = false;
+	enum statusTypes { NADA, HIGH, LOW, CORRECT };
+	int status = NADA;
+	int min = 0;
+	int max = 105;
+	int guess = rand() % 100 + 1;
 
+	std::cout << "Wlecome to my computer guesss game";
+	std::cout << "Where the computer guesses the number the human put in!" << std::endl;
+	std::cout << "Please enter your number (between 1 & 100): " << std::endl;
+	std::cin >> userNumber;
+
+	std::cin.get();
+
+	while (status != CORRECT)
+	{
+		++userTries;
+
+		std::cout << "Computer's Guess: " << guess << "\n" << std::endl;
+
+		std::cout << "Press ENTER to continue..." << std::endl;
+		std::cin.get();
+
+		if (guess < userNumber)
+		{
+			status = LOW;
+		}
+		else if (guess > userNumber)
+		{
+			status = HIGH;
+		}
+		else if (guess == userNumber)
+		{
+			status = CORRECT;
+		}
+
+		if (status == HIGH)
+		{
+			max = guess;
+
+			do
+			{
+				guess = rand() % 100 + 1;
+
+			} while (guess > max || guess < min);
+
+		}
+		else if (status == LOW)
+		{
+			min = guess;
+
+			do
+			{
+				guess = rand() % 100 + 1;
+			} while (guess > max || guess < min);
+
+		}
+
+		else if (status == CORRECT)
+		{
+			std::cout << "The number was " << guess << "." << std::endl;
+			std::cout << "It took the computer " << userTries << " tries!" << std::endl;
+			std::cout << "Thanks for playing!" << std::endl;
+		}
+
+		else
+		{
+			error = true;
+			std::cout << "Somethings wrong" << std::endl;
+		}
+	}
+	system("pause");
+	return 0;
 }
---
+++ Page 203 Number17/Page 203 Number17/Source.cpp
@@ -0,0 +1,4 @@
+// Page 203 Number 17
+// 4-24-18
+// Daniel McGlasson
+
